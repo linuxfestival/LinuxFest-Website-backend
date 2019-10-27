49a7b3ad79c0d4f11eb2d54f603312f4880aaf20
Rozhin <rozhin.st@gmail.com>
2019-10-27 15:50:00 +0330

    userAuth refactored

--- linuxfest-backend/express_middlewares/userAuth.js
+++ linuxfest-backend/express_middlewares/userAuth.js
@@ -1,22 +1,30 @@
 const jwt = require('jsonwebtoken');
 const User = require('../src/models/User');
 
-const auth = async (req, res, next) =>{
-    try{
+async function autCheckUser(req) {
+    try {
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findOne({_id: decoded._id,'tokens.token': token});
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
 
-        if(!user){
-            throw new Error();
+        if (!user) {
+            return false;
         }
 
         req.token = token;
         req.user = user;
-        next();
-    }catch(error){
-        res.status(401).send('Please authenticate');
+        return true
+    } catch (error) {
+        return false;
     }
 }
 
-module.exports = auth;
+const userAuth = async (req, res, next) => {
+        if (await authCheckUser(req)) {
+            next();
+        } else {
+            res.status(401).send('Please authenticate');
+        }
+}
+
+module.exports = userAuth;
