669bd846649ecb27346a61185b8265ae4f2d094b
Mahan Zendedel <43912482+Mahanmmi@users.noreply.github.com>
2019-10-29 11:49:56 +0330

    upload album for workshop added

--- linuxfest-backend/src/routers/workshop.js
+++ linuxfest-backend/src/routers/workshop.js
@@ -1,4 +1,9 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const multer = require('multer');
+const sharp = require('sharp');
+const mongoose = require('mongoose');
 
 const { baseURL } = require('../utils/consts');
 const Workshop = require('../models/Workshop');
@@ -40,7 +45,7 @@ router.get(baseWorkshopUrl, authenticateAdmin, async (req, res) => {
                 participants: workshop.participants
             });
         }
-        
+
         res.send(result);
     } catch (err) {
         res.status(500).send({ error: err.message });
@@ -109,6 +114,99 @@ router.delete(baseWorkshopUrl + '/manage/:id', authenticateAdmin, async (req, re
     await workshop.save();
 
     res.send(workshop);
+});
+
+
+//Upload file endpoint(s)
+const upload = multer({
+    limits: {
+        fileSize: 10000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            cb(new Error('لطفا تصویر آپلود کنید'));
+        }
+        cb(undefined, true);
+    }
+});
+
+router.post(baseWorkshopUrl + '/pic/album/:id', authenticateAdmin, upload.array('pictures'), async (req, res) => {
+    if (!checkPermission(req.admin, 'editWorkshop', res)) {
+        return;
+    }
+
+    try {
+        const workshop = await Workshop.findById(req.params.id);
+        if (!workshop) {
+            res.status(404).send();
+            return;
+        }
+
+        for (const file of req.files) {
+            const buffer = await sharp(file.buffer).resize({ width: 1280, height: 960 }).png().toBuffer();
+            const filePath = path.resolve(path.join("../../uploads", process.env.SITE_VERSION, "workshops", req.params.id, "album"));
+
+            if (!fs.existsSync(filePath)) {
+                fs.mkdirSync(filePath, { recursive: true }, (err) => {
+                    if (err) {
+                        throw new Error(err);
+                    }
+                });
+            }
+
+            const picId = new mongoose.Types.ObjectId();
+            fs.writeFileSync(path.join(filePath, picId.toHexString() + ".png"), buffer, (err) => {
+                if (err) {
+                    throw new Error(err);
+                }
+            });
+
+            workshop.album = workshop.album.concat({
+                _id: picId,
+                albumPicPath: path.join(filePath, picId.toHexString() + ".png")
+            });
+        }
+
+        await workshop.save();
+
+        res.send(workshop);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+}, (err, req, res, next) => {
+    res.status(400).send({ error: err.message });
+});
+
+router.delete(baseWorkshopUrl + '/pic/album/:id/:picid', authenticateAdmin, async (req, res) => {
+    if (!checkPermission(req.admin, 'editWorkshop', res)) {
+        return;
+    }
+
+    try {
+        const workshop = await Workshop.findOne({ _id: req.params.id, 'album._id': req.params.picid });
+        if (!workshop) {
+            res.status(404).send();
+            return;
+        }
+        
+
+        
+        
+        fs.unlinkSync(path.resolve(path.join("../../uploads", process.env.SITE_VERSION, "workshops", req.params.id, "album", req.params.picid + '.png')), (err) => {
+            if(err){
+                throw new Error(err);
+            }
+        });
+
+        workshop.album = workshop.album.filter((picObj) => {
+            return picObj._id.toHexString() !== req.params.picid;
+        });
+        await workshop.save();
+
+        res.send(workshop);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
 });
 
 module.exports = router;
