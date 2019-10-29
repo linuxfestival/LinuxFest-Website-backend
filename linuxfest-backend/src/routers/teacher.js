43dd876f3dd5a896087374b8e5e7e63137285eb3
Mahan Zendedel <43912482+Mahanmmi@users.noreply.github.com>
2019-10-29 10:54:35 +0330

    upload pic for teacher added

--- linuxfest-backend/src/routers/teacher.js
+++ linuxfest-backend/src/routers/teacher.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const multer = require('multer');
+const sharp = require('sharp');
 
 const { baseURL } = require('../utils/consts');
 const Teacher = require('../models/Teacher');
@@ -31,7 +35,7 @@ router.get(baseTeacherUrl, authenticateAdmin, async (req, res) => {
         const teachers = await Teacher.find({});
 
         let result = [];
-        for(const teacher of teachers) {
+        for (const teacher of teachers) {
             await teacher.populate('workshops').execPopulate()
             result = result.concat({
                 teacher,
@@ -113,5 +117,56 @@ router.delete(baseTeacherUrl + '/manage/:id', authenticateAdmin, async (req, res
         res.status(500).send({ error: err.message });
     }
 })
+
+
+//Upload file endpoint(s)
+const upload = multer({
+    limits: {
+        fileSize: 10000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            cb(new Error('لطفا یک تصویر را آپلود کنید'));
+        }
+        cb(undefined, true);
+    }
+});
+
+router.post(baseTeacherUrl + '/pic/:id', authenticateAdmin, upload.single('mainPic'), async (req, res) => {
+    if (!checkPermission(req.admin, 'editTeacher', res)) {
+        return;
+    }
+    try {
+        const teacher = await Teacher.findById(req.params.id);
+        if (!teacher) {
+            res.status(404).send();
+            return;
+        }
+
+        const buffer = await sharp(req.file.buffer).resize({ width: 1280, height: 960 }).png().toBuffer();
+        const filePath = path.resolve(path.join("../../uploads", process.env.SITE_VERSION, "teachers", req.params.id));
+        if (!fs.existsSync(filePath)) {
+            fs.mkdirSync(filePath, { recursive: true }, (err) => {
+                if (err) {
+                    throw new Error(err);
+                }
+            });
+        }
+        fs.writeFileSync(path.join(filePath, "mainPic.png"), buffer, (err) => {
+            if (err) {
+                throw new Error(err);
+            }
+        });
+
+        teacher.imagePath = path.join(filePath, "mainPic.png");
+        await teacher.save();
+
+        res.send(teacher);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+}, (err, req, res, next) => {
+    res.status(400).send({ error: err.message });
+});
 
 module.exports = router;
