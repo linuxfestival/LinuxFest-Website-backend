dfcca583807e71de80af9c68778122a0882651f6
Mahan Zendedel <Mahanmmi@gmail.com>
2019-12-17 21:06:38 +0330

    trying to get cors working

--- linuxfest-backend/src/app.js
+++ linuxfest-backend/src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json());
 
 app.use(require('./routers/superuser'));
