e52c2c9a66c3bff669c9a3ee6ac0b72cfb10f889
Mahan Zendedel <Mahanmmi@gmail.com>
2019-12-18 09:23:57 +0330

    CORS allow method added

--- linuxfest-backend/src/app.js
+++ linuxfest-backend/src/app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
     next();
 });
 
