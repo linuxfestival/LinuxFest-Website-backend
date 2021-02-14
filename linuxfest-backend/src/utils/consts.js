01d464219841807ec796f7acf01deacdd515e954
Ahmad <pyhp2017@gmail.com>
2021-02-14 20:13:15 +0330

    Code Cleaned

--- linuxfest-backend/src/utils/consts.js
+++
@@ -1,9 +0,0 @@
-let baseURL = '';
-if (process.env.SITE_VERSION !== process.env.CURRENT_VERSION) {
-    baseURL = `/${process.env.SITE_VERSION}`;
-}
-
-const initPaymentUrl = "https://sadad.shaparak.ir/api/v0/PaymentByIdentity/PaymentRequest";
-const verifyPaymentUrl = "https://sadad.shaparak.ir/VPG/api/v0/Advice/Verify";
-
-module.exports = { baseURL, initPaymentUrl, verifyPaymentUrl };
