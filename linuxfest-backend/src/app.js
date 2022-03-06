0263abfdf3e7fc49d70075f99db16360f5632bda
alinowrouzii <alinowrouzii@gmail.com>
2022-03-07 01:11:28 +0330

    Add CORS setting

--- linuxfest-backend/src/app.js
+++ linuxfest-backend/src/app.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const { ALLOWED_HOSTS } = process.env;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: String(ALLOWED_HOSTS).trim().split(" ")
+}));
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get("/ping", (req, res) => {
+    res.send("PONG");
+});
 
 
 //================================== Routes ==================================\\
-app.use(`/${process.env.BASEURL}`+'/almightyone', require('./routers/superuser'));
-app.use(`/${process.env.BASEURL}`+'/users', require('./routers/user'));
-app.use(`/${process.env.BASEURL}`+'/workshops', require('./routers/workshop'));
-app.use(`/${process.env.BASEURL}`+'/teachers', require('./routers/teacher'));
-app.use(`/${process.env.BASEURL}`+'/discounts', require('./routers/discount'));
-app.use(`/${process.env.BASEURL}`+'/statics', require('./routers/static'));
-app.use(`/${process.env.BASEURL}`+'/companies', require('./routers/company'))
+app.use(`/${process.env.BASEURL}` + '/almightyone', require('./routers/superuser'));
+app.use(`/${process.env.BASEURL}` + '/users', require('./routers/user'));
+app.use(`/${process.env.BASEURL}` + '/workshops', require('./routers/workshop'));
+app.use(`/${process.env.BASEURL}` + '/teachers', require('./routers/teacher'));
+app.use(`/${process.env.BASEURL}` + '/discounts', require('./routers/discount'));
+app.use(`/${process.env.BASEURL}` + '/statics', require('./routers/static'));
+app.use(`/${process.env.BASEURL}` + '/companies', require('./routers/company'))
 
 module.exports = app;
